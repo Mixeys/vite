5eaa0aac788c9ba3341552233c2edc5b47ed74a0
Mykhailo Petrov <mykhailo.petrov@schindler.com>
2024-01-07 22:50:17 +0200

    Working with styles in vite

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './src/2.js'
+import './src/assets/5.css'
+import './src/assets/3.css'
+import { heading as headingColor } from './src/assets/4a.module.css'
+import { heading as headingSize } from './src/assets/4b.module.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// import styles from './src/assets/3.css?inline'
 
-setupCounter(document.querySelector('#counter'))
+// const stylesEl = document.createElement('style')
+
+// stylesEl.innerText = styles
+
+// document.querySelector('head').append(stylesEl)
+
+document.querySelector('.heading').className = `${headingColor} ${headingSize}`
